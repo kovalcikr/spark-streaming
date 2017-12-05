46c3dad17f0c1f5070d574636945006bfa2831ca
Radovan Kovalcik <radovankovalcik@gmail.com>
2017-12-05 22:25:41 +0100

    complete big put

--- src/main/java/test/BigPut.java
+++ src/main/java/test/BigPut.java
@@ -2,6 +2,8 @@ package test;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
@@ -10,6 +12,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.apache.hadoop.hbase.util.Bytes.toBytes;
@@ -19,28 +22,51 @@ import static org.apache.hadoop.hbase.util.Bytes.toBytes;
  */
 public class BigPut {
 
-    public static final int MAX_VALUE = 1_000_000_000;
+    public static final int BATCH_COUNT = 10_000;
+    public static final int BATCH_SIZE = 100_000;
 
-    private static String tableName;
+    private static String tableNameString;
 
     public static void main(String[] args) throws IOException {
         SparkConf conf = new SparkConf().setMaster("local[*]").setAppName("test");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         Configuration hbaseConf = HBaseConfiguration.create();
-        tableName = "test_name";
-        hbaseConf.set(TableOutputFormat.OUTPUT_TABLE, tableName);
+        tableNameString = "test_name";
+        hbaseConf.set(TableOutputFormat.OUTPUT_TABLE, tableNameString);
+
+        TableName tableName = TableName.valueOf(tableNameString);
 
         Connection connection = ConnectionFactory.createConnection(hbaseConf);
-        try (Table table = connection.getTable(TableName.valueOf(tableName))) {
-            List<Put> puts = new ArrayList<>();
-            for (long i = 0; i < MAX_VALUE; i++) {
-                Put put = new Put(toBytes("TestRow" + i));
-                put.addColumn(toBytes("a"), toBytes("test"), toBytes("some value " + i));
-                puts.add(put);
-            }
-            table.put(puts);
+
+        Admin admin = connection.getAdmin();
+
+        System.out.println("Tables");
+        Arrays.stream(admin.listTableNames()).map(TableName::getNameAsString).forEach(System.out::println);
+        System.out.println();
+
+        if (!admin.tableExists(tableName)) {
+            System.out.println("creating table");
+            HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
+            hTableDescriptor.addFamily(new HColumnDescriptor("a"));
+            admin.createTable(hTableDescriptor);
         }
+
+        try (Table table = connection.getTable(tableName)) {
+            for (long i = 0; i < BATCH_COUNT; i++) {
+                List<Put> puts = new ArrayList<>();
+                for (long j = 0; j < BATCH_SIZE; j++) {
+                    long num = i * j + j;
+                    Put put = new Put(toBytes("TestRow" + num));
+                    put.addColumn(toBytes("a"), toBytes("test"), toBytes("some value " + num));
+                    puts.add(put);
+                }
+                long time = System.currentTimeMillis();
+                table.put(puts);
+                System.out.println("writed: " + ((double) i / BATCH_COUNT * 100) + " in " + (System.currentTimeMillis() - time));
+            }
+        }
+
         connection.close();
         sc.stop();
     }
